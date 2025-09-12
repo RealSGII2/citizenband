e363af41008693fcbdeb4c3b7db235ae7ac9b2e3
William <william@wwise.dev>
2025-09-12 01:36:21 -0500

    Make postprocessing slider change influence properly

--- web/src/app/app/server/[serverId]/effects.ts
+++ web/src/app/app/server/[serverId]/effects.ts
@@ -1,91 +1,123 @@
+function lerp(a: number, b: number, x: number) {
+  return a + (b - a) * x;
+}
+
+const n_samples = 44100;
+const distortionIdentity = new Float32Array(n_samples);
+
+for (let i = 0; i < n_samples; ++i)
+  distortionIdentity[i] = (i / (n_samples - 1)) * 2 - 1;
+
+function makeDistortionCurve(
+  amount: number,
+  multiplier: number,
+): Float32Array<ArrayBuffer> {
+  const curve = new Float32Array(n_samples);
+  const deg = Math.PI / 180;
+  for (let i = 0; i < n_samples; ++i) {
+    const x = (i * 2) / n_samples - 1;
+    curve[i] =
+      ((3 + amount) * x * 20 * deg) / (Math.PI + amount * Math.abs(x));
+  }
+  return curve.map((x, i) => lerp(distortionIdentity[i], x, multiplier));
+}
+
 export default function addPostProcessing(
   stream: MediaStream,
   volume: number,
-  postProcessing: number
+  postProcessing: number,
 ): {
-  track: MediaStreamTrack
-  adjustPostProcessing(amount: number): void
-  adjustVolume(amount: number): void
+  track: MediaStreamTrack;
+  adjustPostProcessing(amount: number): void;
+  adjustVolume(amount: number): void;
 } {
-  const workaround = new Audio()
-  workaround.srcObject = stream
-  workaround.muted = true
-  requestAnimationFrame(() => workaround.remove())
+  const workaround = new Audio();
+  workaround.srcObject = stream;
+  workaround.muted = true;
+  requestAnimationFrame(() => workaround.remove());
 
-  const context = new window.AudioContext()
-  const source = context.createMediaStreamSource(stream)
+  const context = new window.AudioContext();
+  const source = context.createMediaStreamSource(stream);
 
-  const bandpass = context.createBiquadFilter()
-  bandpass.type = 'bandpass'
-  bandpass.frequency.value = 1500
-  bandpass.Q.value = postProcessing
+  const bandpass = context.createBiquadFilter();
+  bandpass.type = "bandpass";
+  bandpass.frequency.value = 1500;
+  bandpass.Q.value = postProcessing;
 
-  const highpass = context.createBiquadFilter()
-  highpass.type = 'highpass'
-  highpass.frequency.value = 300 * postProcessing
+  const preBandPassGain = context.createGain();
+  preBandPassGain.gain.value = 1 - postProcessing;
 
-  const highshelf = context.createBiquadFilter()
-  highshelf.type = 'highshelf'
-  highshelf.frequency.value = 4000
-  highshelf.gain.value = -17.9 * postProcessing
+  const postBandPassGain = context.createGain();
+  postBandPassGain.gain.value = postProcessing;
 
-  const lowshelf = context.createBiquadFilter()
-  lowshelf.type = 'lowshelf'
-  lowshelf.frequency.value = 400
-  lowshelf.gain.value = -17.9 * postProcessing
+  const highpass = context.createBiquadFilter();
+  highpass.type = "highpass";
+  highpass.frequency.value = 300 * postProcessing;
 
-  function makeDistortionCurve(amount = 20): Float32Array<ArrayBuffer> {
-    const n_samples = 44100
-    const curve = new Float32Array(n_samples)
-    const deg = Math.PI / 180
-    for (let i = 0; i < n_samples; ++i) {
-      const x = (i * 2) / n_samples - 1
-      curve[i] = ((3 + amount) * x * 20 * deg) / (Math.PI + amount * Math.abs(x))
-    }
-    return curve
-  }
+  const highshelf = context.createBiquadFilter();
+  highshelf.type = "highshelf";
+  highshelf.frequency.value = 4000;
+  highshelf.gain.value = -17.9 * postProcessing;
 
-  const distortion = context.createWaveShaper()
-  distortion.curve = makeDistortionCurve(50)
-  distortion.oversample = '4x'
+  const lowshelf = context.createBiquadFilter();
+  lowshelf.type = "lowshelf";
+  lowshelf.frequency.value = 400;
+  lowshelf.gain.value = -17.9 * postProcessing;
 
-  const wetGain = context.createGain()
-  wetGain.gain.value = postProcessing
+  const distortion = context.createWaveShaper();
+  distortion.curve = makeDistortionCurve(50, postProcessing);
+  distortion.oversample = "4x";
 
-  const dryGain = context.createGain()
-  dryGain.gain.value = 1 - postProcessing
+  const wetGain = context.createGain();
+  // wetGain.gain.value = postProcessing
+  wetGain.gain.value = postProcessing * 0.25 + 0.75;
 
-  source.connect(bandpass)
-  bandpass.connect(highpass)
-  highpass.connect(highshelf)
-  highshelf.connect(lowshelf)
-  lowshelf.connect(distortion)
-  distortion.connect(wetGain)
+  const dryGain = context.createGain();
+  // dryGain.gain.value = 1 - postProcessing
+  dryGain.gain.value = 0;
 
-  source.connect(dryGain)
+  source.connect(preBandPassGain);
 
-  const finalGain = context.createGain()
-  finalGain.gain.value = volume
+  source.connect(bandpass);
+  bandpass.connect(highpass);
+  // bandpass.connect(postBandPassGain)
+  //
+  // preBandPassGain.connect(highpass)
+  // postBandPassGain.connect(highpass)
 
-  dryGain.connect(finalGain)
-  wetGain.connect(finalGain)
+  highpass.connect(highshelf);
+  highshelf.connect(lowshelf);
+  lowshelf.connect(distortion);
+  distortion.connect(wetGain);
 
-  const final = context.createMediaStreamDestination()
-  finalGain.connect(final)
+  source.connect(dryGain);
+
+  const finalGain = context.createGain();
+  finalGain.gain.value = volume;
+
+  dryGain.connect(finalGain);
+  wetGain.connect(finalGain);
+
+  const final = context.createMediaStreamDestination();
+  finalGain.connect(final);
 
   return {
     track: final.stream.getAudioTracks()[0],
     adjustPostProcessing(amount: number) {
-      bandpass.Q.value = amount
-      highpass.frequency.value = 300 * amount
-      highshelf.gain.value = -17.9 * amount
-      lowshelf.gain.value = -17.9 * amount
+      bandpass.Q.value = amount;
+      highpass.frequency.value = 300 * amount;
+      highshelf.gain.value = -17.9 * amount;
+      lowshelf.gain.value = -17.9 * amount;
 
-      dryGain.gain.value = 1 - amount
-      wetGain.gain.value = amount
+      distortion.curve = makeDistortionCurve(50, amount);
+
+      // dryGain.gain.value = 1 - amount
+      wetGain.gain.value = amount * 0.25 + 0.75;
+      preBandPassGain.gain.value = 1 - amount;
+      postBandPassGain.gain.value = amount;
     },
     adjustVolume(amount: number) {
-      finalGain.gain.value = amount
-    }
-  }
+      finalGain.gain.value = amount;
+    },
+  };
 }
