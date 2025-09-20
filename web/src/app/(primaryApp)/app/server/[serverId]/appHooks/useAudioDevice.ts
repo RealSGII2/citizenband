f9a56ef1c0a157b7f5afde0d393956b80b096f27
William <william@wwise.dev>
2025-09-19 23:16:59 -0500

    Disable automatic gain control

--- web/src/app/(primaryApp)/app/server/[serverId]/appHooks/useAudioDevice.ts
+++ web/src/app/(primaryApp)/app/server/[serverId]/appHooks/useAudioDevice.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import type { DailyCall } from '@daily-co/daily-js'
+import { useEffect, useState } from 'react';
+import type { DailyCall } from '@daily-co/daily-js';
 
 async function fetchDevicesAsync() {
   return (await navigator.mediaDevices.enumerateDevices()).filter((x) => x.kind == 'audioinput')
@@ -8,6 +8,14 @@ async function fetchDevicesAsync() {
 export default function useAudioDevice(callObj: DailyCall) {
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([])
   const [currentDevice, setCurrentDevice] = useState<MediaDeviceInfo | null>(null)
+
+  useEffect(() => {
+    (async () => {
+      await callObj.setInputDevicesAsync({
+        audioSource: (await navigator.mediaDevices.getUserMedia({ audio: { deviceId: currentDevice?.deviceId, autoGainControl: false } })).getAudioTracks()[0]
+      })
+    })()
+  }, [callObj, currentDevice]);
 
   useEffect(() => {
     ;(async () => {
@@ -33,10 +41,11 @@ export default function useAudioDevice(callObj: DailyCall) {
     async setCurrentDeviceIdAsync(newDeviceId: string) {
       const newDevice = devices.find((x) => x.deviceId == newDeviceId)
       if (!newDevice) return
-
-      await callObj.setInputDevicesAsync({
-        audioDeviceId: newDeviceId
-      })
+      //
+      // await callObj.setInputDevicesAsync({
+      //   audioDeviceId: newDeviceId,
+      //   audioSource
+      // })
       localStorage.setItem('selectedAudioInput', newDeviceId)
       setCurrentDevice(newDevice)
     }
